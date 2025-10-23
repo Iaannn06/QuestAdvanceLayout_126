ee6150f53e8aa72d5ec2c645d1081edcde17c6e9
Iaannn06 <161089330+Iaannn06@users.noreply.github.com>
2025-10-23 15:28:51 +0700

    menambahkan modifier untuk column pertama

--- app/src/main/java/com/example/week3/UIPertama.kt
+++ app/src/main/java/com/example/week3/UIPertama.kt
@@ -6,5 +6,10 @@ import androidx.compose.ui.Modifier
 @Composable
 fun ActivitasPertama(modifier: Modifier) {
     Column (modifier = Modifier
-    )
+        .padding(top=100.dp)
+        .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally)
+    ) {
+
+    }
 }
